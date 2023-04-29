b3fa0df65af77d8479277e839953fadd591ef1d0
huy <voquanghuy08102000@gmail.com>
2023-04-30 00:31:07 +0700

    update entity and navbar

---
+++ BOOKINGTOUR/src/main/java/BOOKINGTOUR/entity/KhuyenMai.java
@@ -0,0 +1,102 @@
+package BOOKINGTOUR.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+
+@Entity
+@Table(name="KHUYENMAI")
+public class KhuyenMai {
+	@Id
+	@Column(name="MAKM")
+	private  int maKM;
+	@Column(name="TEN")
+	private  String ten;
+	@Column(name="MOTA")
+	private  String moTa;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Column(name="TGBD")
+	private  Date tGBD;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Column(name="TGKT")
+	private  Date tGKT;
+	@Column(name="SOTIENGIAM")
+	private  int soTienGiam;
+	
+	@OneToMany(mappedBy = "khuyenMai", fetch = FetchType.EAGER)
+	private List<CTVe> cTve;
+
+	public int getMaKM() {
+		return maKM;
+	}
+
+	public void setMaKM(int maKM) {
+		this.maKM = maKM;
+	}
+
+	public String getTen() {
+		return ten;
+	}
+
+	public void setTen(String ten) {
+		this.ten = ten;
+	}
+
+	public String getMoTa() {
+		return moTa;
+	}
+
+	public void setMoTa(String moTa) {
+		this.moTa = moTa;
+	}
+
+	public Date gettGBD() {
+		return tGBD;
+	}
+
+	public void settGBD(Date tGBD) {
+		this.tGBD = tGBD;
+	}
+
+	public Date gettGKT() {
+		return tGKT;
+	}
+
+	public void settGKT(Date tGKT) {
+		this.tGKT = tGKT;
+	}
+
+	public int getSoTienGiam() {
+		return soTienGiam;
+	}
+
+	public void setSoTienGiam(int soTienGiam) {
+		this.soTienGiam = soTienGiam;
+	}
+
+	public List<CTVe> getcTve() {
+		return cTve;
+	}
+
+	public void setcTve(List<CTVe> cTve) {
+		this.cTve = cTve;
+	}
+	
+
+}
---
+++ BOOKINGTOUR/src/main/java/BOOKINGTOUR/entity/LoaiLuuTru.java
@@ -0,0 +1,42 @@
+package BOOKINGTOUR.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TAIKHOAN")
+public class LoaiLuuTru {
+	@Id
+	@Column(name="MALLT")
+	private  int maLLT;
+	@Column(name="TENLOAILUUTRU")
+	private  String tenLoaiLuuTru;
+	@OneToMany(mappedBy = "loaiLuuTru", fetch = FetchType.EAGER)
+	private List<NoiLuuTru> noiLuuTru;
+	
+	public int getMaLLT() {
+		return maLLT;
+	}
+	public void setMaLLT(int maLLT) {
+		this.maLLT = maLLT;
+	}
+	public String getTenLoaiLuuTru() {
+		return tenLoaiLuuTru;
+	}
+	public void setTenLoaiLuuTru(String tenLoaiLuuTru) {
+		this.tenLoaiLuuTru = tenLoaiLuuTru;
+	}
+	public List<NoiLuuTru> getNoiLuuTru() {
+		return noiLuuTru;
+	}
+	public void setNoiLuuTru(List<NoiLuuTru> noiLuuTru) {
+		this.noiLuuTru = noiLuuTru;
+	}
+	
+}
